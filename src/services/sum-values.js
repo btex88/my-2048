e3b5dfd6eec25e492fa1d5884ca657148d9f0ac6
bruno teixeira <btex88@gmail.com>
2022-01-24 12:00:38 -0300

    refactor(sum-values.js): little changes that don't affect the code

--- src/services/sum-values.js
+++ src/services/sum-values.js
@@ -1,21 +1,17 @@
 function sumValues(arr, direction) {
-  if (direction === 'right') {
-    console.log('entrei');
-    return arr.reduce((acc, row) => {
-      const newRow = row.reduce((accum, value, index) => {
-        if (value === row[index - 1]) {
-          accum.push((value + row[index - 1]));
-          accum.splice((index - 1), 1, 0);
-        } else {
-          accum.push(value);
-        }
-        return accum;
-      }, []);
-      acc.push(newRow);
-      return acc;
+  return arr.reduce((acc, row) => {
+    const newRow = row.reduce((accum, value, index) => {
+      if (direction === 'right' && value === row[index - 1]) {
+        accum.push((value + row[index - 1]));
+        accum.splice((index - 1), 1, 0);
+      } else {
+        accum.push(value);
+      }
+      return accum;
     }, []);
-  }
-  return arr;
+    acc.push(newRow);
+    return acc;
+  }, []);
 }
 
 export default sumValues;
