18f7c912918dfbc91e6137b1db0332e55a7a04ee
bruno teixeira <btex88@gmail.com>
2022-02-10 20:56:55 -0300

    fix(swipe-left): implemented solution to clean previous index after swipe left action

--- src/services/sum-values.js
+++ src/services/sum-values.js
@@ -16,9 +16,10 @@ export function sumRight(arr) {
 
 export function sumLeft(arr) {
   return arr.reduce((acc, row) => {
-    const newRow = row.reduce((coll, curr, index) => {
+    const newRow = row.reduce((coll, curr, index, currArr) => {
       if (curr === row[index + 1] && index < 3) {
         coll.push((curr + row[index + 1]));
+        currArr.splice((index + 1), 1, '');
       } else {
         coll.push(curr);
       }
